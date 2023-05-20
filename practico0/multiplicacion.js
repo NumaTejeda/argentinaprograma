936482ff9f4e025af28491c4781cf1301f404155
Nico <at.psicotrisquel@gmail.com>
2023-05-20 11:33:34 -0300

    resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,4 @@
+function multiplicacion(num1, num2, num3){
+    return console.log("El resultado es: " + num1 * num2 * num3);
+}
+multiplicacion(2, 17, 6);
---
+++ practico0/resta.js
@@ -0,0 +1,5 @@
+function resta(num1, num2){
+    return console.log(num1 - num2);
+}
+
+resta(10, 2);
