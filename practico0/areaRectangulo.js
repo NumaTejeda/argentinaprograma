d4c04608ecf09be09ec6ffd2cff9500ace4bb45c
Nico <at.psicotrisquel@gmail.com>
2023-05-20 11:41:42 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,6 @@
+function areaRectangulo(base, altura){
+    return console.log("El area es: " + base * altura);
+}
+
+areaRectangulo(5, 3);
+areaRectangulo(12.5, 5.3);
---
+++ practico0/promedio3.js
@@ -0,0 +1,8 @@
+function promedio(num1, num2, num3){
+    let resultado = (num1 + num2 + num3)/3;
+    return console.log("El promedio es: " + resultado);
+}
+
+promedio(5, 7, 9);
+promedio(-2, -3, -8);
+promedio(-1.2, 5.3, -3.3);
